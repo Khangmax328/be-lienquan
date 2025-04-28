a484e48892a56826df225b3a41437da6505fd19a
khangmax328 <phimaxcf328@gmail.com>
2025-04-28 15:17:56 +0700

    Fix domain

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Sử dụng CORS cho toàn bộ ứng dụng
 // app.use(cors());  // Mở cho tất cả các domain (có thể cấu hình chi tiết nếu cần)
 const allowedOrigins = [
-  "http://localhost:3000",
+  "http://shopkhanglienquan.com'",
   "https://fe-lienquan.onrender.com"
 ];
 app.use(cors({
@@ -33,4 +33,3 @@ require('./src/routes')(app);  // Đảm bảo tất cả route được định
 app.listen(PORT, () => {
   console.log(`Server chạy tại http://localhost:${PORT}`);
 });
-console.log("Backend running");
