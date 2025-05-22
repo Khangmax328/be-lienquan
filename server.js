4c728c6080a62f2709aecb0f0428db3484dd3652
khangmax328 <phimaxcf328@gmail.com>
2025-05-22 18:04:07 +0700

    fix cau hinh

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const dotenv = require('dotenv');
-dotenv.config();  // Đọc các giá trị từ file .env
-require('./src/config/db');  // Kết nối MongoDB
+dotenv.config();
 require('./src/config/db')();
 const express = require('express');
-const cors = require('cors');  // Import cors
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Sử dụng CORS cho toàn bộ ứng dụng
-// app.use(cors());  // Mở cho tất cả các domain (có thể cấu hình chi tiết nếu cần)
 const allowedOrigins = [
   "http://localhost:3000",
   "http://shopkhanglienquan.com",
@@ -19,6 +16,16 @@ const allowedOrigins = [
   "https://lienquanshop-97dac72c4429.herokuapp.com"
 ];
 
+// Middleware redirect domain gốc sang www
+app.use((req, res, next) => {
+  const host = req.headers.host;
+  if (host === 'shopkhanglienquan.com') {
+    return res.redirect(301, 'https://www.shopkhanglienquan.com' + req.originalUrl);
+  }
+  next();
+});
+
+// CORS
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -30,11 +37,11 @@ app.use(cors({
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 }));
-// Cấu hình middleware
-app.use(express.json());  // Để xử lý các request body kiểu JSON
+
+app.use(express.json());
 
 // Các route khác
-require('./src/routes')(app);  // Đảm bảo tất cả route được định nghĩa
+require('./src/routes')(app);
 
 app.listen(PORT, () => {
   console.log(`Server chạy tại http://localhost:${PORT}`);
